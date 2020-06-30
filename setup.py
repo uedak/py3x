1435daed8fe1f730968be0cdbfedd62970afb1bc
uedak <uedak@r.recruit.co.jp>
2020-06-30 14:14:52 +0900

    0.1.6

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages, setup
 setup(
     name='py3x',
-    version='0.1.5',
+    version='0.1.6',
     description='python3 extension',
     url='https://github.com/uedak/py3x',
     author='uedak',
